c5d6a3d37776273c19f3ad6c2a523a4605429945
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-26 16:48:51 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? lemmy_dependencies="postgresql postgresql-contrib"
-#REMOVEME? lemmyui_dependencies="espeak"
-
-#REMOVEME? pkg_dependencies="$lemmy_dependencies $lemmyui_dependencies"
-
 NODEJS_VERSION=12
 
 #=================================================
